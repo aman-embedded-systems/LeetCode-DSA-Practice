f70cbd9da297b854c89bf48cb95a1c50da63e470
aman-embedded-systems <aman.hbtu.kampur@gmail.com>
2026-07-02 21:26:22 +0530

    Time: 134 ms (6%), Space: 8.6 MB (65.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         int numcpy=x;
+    long palindromenum=0;
+    if(x==0)
+    {
+        return 1;
+    }
+    else if(x<0)
+    {
+        return false;
+    }
+    while(numcpy)
+    {
+        if(palindromenum*10 > INT_MAX || palindromenum < INT_MIN) {
+              return false; 
+         }
+        palindromenum=(palindromenum*10)+(numcpy%10);
+
+        numcpy/=10;
+        printf("%d: \n",palindromenum);
+    }
+   
+    return (palindromenum==x);
+    }
+};
