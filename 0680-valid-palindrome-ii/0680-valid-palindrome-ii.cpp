777301540c77213f1dba125db1e34d1046be4b76
aman-embedded-systems <aman.hbtu.kampur@gmail.com>
2026-07-06 22:11:30 +0530

    Time: 0 ms (100%), Space: 22 MB (92.97%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // Helper function to check if a substring is a pure palindrome
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // If a mismatch occurs, try skipping either s[left] or s[right]
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
