4cf96b582b181cd0af659c66d8cfe9e932c737b2
aman-embedded-systems <aman.hbtu.kampur@gmail.com>
2026-07-10 17:29:46 +0530

    Time: 0 ms (100%), Space: 9.5 MB (80.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.c
+++ 0268-missing-number/0268-missing-number.c
@@ -6,6 +6,7 @@ int cmp(void const *a,void const *b)
     return (ia>ib)-(ia<ib);
 }
 int missingNumber(int* nums, int numsSize) {
+#if 0
     int n=numsSize;
     qsort(nums,numsSize,sizeof(int),cmp);
     for(int idx=0;idx<n;idx++)
@@ -16,4 +17,12 @@ int missingNumber(int* nums, int numsSize) {
         }
     }
     return numsSize;
+#endif 
+    int n=numsSize;
+    int missingno=n;
+    for(int idx=0;idx<n;idx++)
+    {
+        missingno=missingno^(nums[idx]^idx);
+    }
+    return missingno;
 }
